86abfd550cd26c8ef5e08ed94408bf4f79e9ede6
GumBlor <gumblor163@gmail.com>
2023-10-30 19:18:43 +0500

    Отправка координат конкретному пользователю

--- CoordinateReader.py
+++ CoordinateReader.py
@@ -1,6 +1,6 @@
 '''
 Идея программы.
-* Поступает запрос на местоположение определенного человека. Этому человеку бот скидывает сообщение.
+* Поступает запрос на местоположение определенного человека. Этому человеку (по его id или нику) скидывает сообщение.
 * Человек отправляет свои координаты. При отправлении координат записываются в БД широта и долгота.
 * Запрашиваемому скидываются координаты посредством bot.send_location(message.chat.id, *latitude*, *longitude*)
 '''
@@ -17,12 +17,13 @@ def main(message):
     keyboard.add(types.KeyboardButton(text="Отправить местоположение", request_location=True))
     # Предложение отправить свои координаты
     bot.send_message(message.chat.id, "Отправьте свои коордианты!", reply_markup=keyboard)
-
     # Выполнение метода click после нажатия кнопки
     bot.register_next_step_handler(message, click)
 
 def click(message):
-    # Просто отправляется сообщение с координатами пользователя
-    bot.send_message(message.chat.id, f'latitude: {message.location.latitude}, longitude: {message.location.longitude}')
+    # При нажатии на кнопку человек с заданным ID скидывается местоположение со словами "Он спалился!"
+    userID = 514538959
+    bot.send_message(userID, 'Он спалился!')
+    bot.send_location(userID, message.location.latitude, message.location.longitude)
 
 bot.polling(non_stop=True)
