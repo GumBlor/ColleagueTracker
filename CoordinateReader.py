114d4097f1191745a875dce83df487a80a1fcf86
GumBlor <gumblor163@gmail.com>
2023-11-04 19:48:38 +0500

    Уточнения

--- CoordinateReader.py
+++ CoordinateReader.py
@@ -1,28 +1,19 @@
-'''
-Идея программы.
-* Поступает запрос на местоположение определенного человека. Этому человеку (по его id) скидывает сообщение.
-* Человек отправляет свои координаты. При отправлении координат записываются в БД широта и долгота.
-* Запрашиваемому скидываются координаты посредством bot.send_location(message.chat.id, *latitude*, *longitude*)
-'''
-
 import telebot
 from telebot import types
 
 bot = telebot.TeleBot('6447415648:AAHAZrEnHgt7Vtlq8aaptxLuGUd4ZlQCLig')
 
-'''
-* Запрос. В вашу строку в БД вставляется id человека, которому необходимо ваше местоположение
-* Постоянная проверка собственной строки. Если стобец с id вызвавшего запрос человека
-не равен нулю, то отправить себе сообщение с предложением отправить ему координаты
-* Отправка собственной геолокации по известному id
-* Зануление строки с id вызвавшему запрос человека   
-'''
 senderID = ''
 
 @bot.message_handler(commands=["start"])
 def registration(message):
     # Внести в базу данных основные сведения о человеке, который только включил бота
-    bot.send_message(message.chat.id, 'ФИО?')
+    bot.send_message(message.chat.id, 'Введите вашу фамилию')
+    '''
+        Создание строки с двумя столбцами:
+            * message.text (фамилия)
+            * message.from_user.id (id пользователя)
+    '''
     # Создание кнопки
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     keyboard.add(types.KeyboardButton(text="Отправить местоположение", request_location=True))
@@ -35,13 +26,17 @@ def send(message):
         bot.send_message(senderID, f'Пользователь {message.from_user.id} находится здесь:')
         bot.send_location(senderID, message.location.latitude, message.location.longitude)
         # Если я отправлю при запросе от меня же, то мне придет моя геолокация
-        # Поэтому необходимо занулить эту строку
+        # Поэтому необходимо занулить эту строку:
         senderID = ''
 
 @bot.message_handler(commands=["find"])
 def find(message):
     # Просьба указать интересуемого коллегу
-    bot.send_message(message.chat.id, 'Напишите id человека, которого вы хотите найти') # Запрос должен быть по имени/должности!
+    bot.send_message(message.chat.id, 'Напишите id человека, которого вы хотите найти')
+    '''
+        Здесь необходимо запросить местоположение по фамилии, по ней находится строка в базе данных.
+        В соседнем столбце находится id человека
+    '''
     # Отправить ему уведомление
     bot.register_next_step_handler(message, notification)
 
